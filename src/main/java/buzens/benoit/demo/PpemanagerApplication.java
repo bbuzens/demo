2002b8cd1984766f75cc7ef3ad5a8024b7c93523
buzensb <benoit.buzens@sesam-vitale.fr>
2025-08-06 16:30:23 +0200

    Initial commit

---
+++ src/main/java/buzens/benoit/demo/PpemanagerApplication.java
@@ -0,0 +1,13 @@
+package buzens.benoit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpemanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/buzens/benoit/demo/PpemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package buzens.benoit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
