467a942052181836916eb0a179afe34a46019627
buzensb <benoit.buzens@sesam-vitale.fr>
2025-08-12 09:28:04 +0200

    Datasource config and security adjustments

---
+++ src/main/java/buzens/benoit/demo/PpemanagerStatusController.java
@@ -0,0 +1,17 @@
+package buzens.benoit.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PpemanagerStatusController {
+    Logger logger = LoggerFactory.getLogger(PpemanagerApplication.class);
+
+    @GetMapping(value = "/status")
+    public String status() {
+        logger.info("Status");
+        return "OK";
+    }
+}
